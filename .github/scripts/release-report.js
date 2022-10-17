16e4140acb4a5654a8aa51a9baa5a7347ab77fbb
Suraj Auwal <siradjiawoual@gmail.com>
2022-10-17 06:48:32 +0100

    chore(ci): update dockerfiles and build commands on CI (#74)
    * chore(ci): update dockerfiles and build commands on CI
    
    * chore(ci): fix scirpt path

---
+++ .github/scripts/release-report.js
@@ -0,0 +1,29 @@
+
+/**
+ * Release Report
+ *
+ * Generate a release message in PRs reporting the success of a new build
+ * along with some quality of life links.
+ */
+module.exports = ({ context }) => {
+    const prLinks = getContainerPRLinks(context)
+    const hashLinks = getContainerHashLinks(context)
+    return `
+Docker build preview for aristotle/apps is ready!
+          
+Built with commit ${context.sha}
+ - ${prLinks.join('\n - ')}
+You can also get an immutable image with the commit hash
+ - ${hashLinks.join('\n - ')}
+`
+}
+
+function getContainerPRLinks({ payload: { number } }) {
+    const apps = process.env.APPS.split(',')
+    return apps.map(app => `ghcr.io/imagyne/eatlater-${app}:pr-${number}`)
+}
+
+function getContainerHashLinks({ sha }) {
+    const apps = process.env.APPS.split(',')
+    return apps.map(app => `ghcr.io/imagyne/eatlater-${app}:${sha}`)
+}
