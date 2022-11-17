20349516ad052137925cb90f7dd407af7612893a
siradji <siradjiawoual@gmail.com>
2022-11-17 11:39:20 +0100

    [WIP] review service

---
+++ libs/common/src/database/dto/review.dto.ts
@@ -0,0 +1,26 @@
+import { IsNotEmpty, IsString, IsUUID } from 'class-validator'
+
+export class ReviewDto {
+
+    @IsUUID(4)
+    @IsNotEmpty()
+    public listingId: string
+
+
+    @IsUUID(4)
+    @IsNotEmpty()
+    public orderId: string
+
+    @IsString()
+    @IsNotEmpty()
+    public  reviewBody: string
+
+    @IsUUID(4)
+    @IsNotEmpty()
+    public vendorId: string
+
+
+    @IsString()
+    @IsNotEmpty()
+    public reviewStars: number
+}
---
+++ libs/common/src/database/entities/Reviews.ts
@@ -0,0 +1,33 @@
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm'
+
+@Entity('reviews')
+export class ReviewEntity {
+
+    @PrimaryGeneratedColumn('uuid')
+    public id: string
+
+    @Column({type: 'text'})
+    public  reviewBody: string
+
+    @Column({type: 'uuid'})
+    public listingId: string
+
+    @Column({type: 'uuid'})
+    public vendorId: string
+
+    @Column({type: 'uuid'})
+    public orderId: string
+
+    @Column({type: 'int'})
+    public reviewStars: number
+
+    @UpdateDateColumn()
+    public updatedAt: Date
+
+    @CreateDateColumn()
+    public createdAt: Date
+
+    @DeleteDateColumn()
+    public deletedAt: Date
+
+}
