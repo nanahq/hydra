c071222719ca82e22bddfeb45e430883c812738a
ahmard <ahmard.mu@gmail.com>
2022-11-27 10:02:41 +0100

    style(codebase): reformatted codebase

--- apps/admin-gateway/src/module.api/reviews.controller.ts
+++ apps/admin-gateway/src/module.api/reviews.controller.ts
@@ -1,8 +1,22 @@
-import { Controller, Delete, Get, HttpException, Inject, Param, UseGuards } from '@nestjs/common'
+import {
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  Inject,
+  Param,
+  UseGuards
+} from '@nestjs/common'
 import { ClientProxy } from '@nestjs/microservices'
 import { catchError, lastValueFrom } from 'rxjs'
 
-import { IRpcException, QUEUE_MESSAGE, QUEUE_SERVICE, ResponseWithStatus, ReviewEntity } from '@app/common'
+import {
+  IRpcException,
+  QUEUE_MESSAGE,
+  QUEUE_SERVICE,
+  ResponseWithStatus,
+  ReviewEntity
+} from '@app/common'
 import { JwtAuthGuard } from '../auth/guards/jwt.guard'
 
 @Controller('reviews')
@@ -16,12 +30,14 @@ export class ReviewsController {
   @Get('')
   async getAllReviews (): Promise<ReviewEntity[]> {
     return await lastValueFrom<ReviewEntity[]>(
-      this.reviewsClient.send(QUEUE_MESSAGE.REVIEW_ADMIN_GET_ALL_IN_DB, {}).pipe(
-        catchError((error: IRpcException) => {
-          console.log(error)
-          throw new HttpException(error.message, error.status)
-        })
-      )
+      this.reviewsClient
+        .send(QUEUE_MESSAGE.REVIEW_ADMIN_GET_ALL_IN_DB, {})
+        .pipe(
+          catchError((error: IRpcException) => {
+            console.log(error)
+            throw new HttpException(error.message, error.status)
+          })
+        )
     )
   }
 
@@ -43,11 +59,13 @@ export class ReviewsController {
     @Param('id') reviewId: string
   ): Promise<ResponseWithStatus> {
     return await lastValueFrom<ResponseWithStatus>(
-      this.reviewsClient.send(QUEUE_MESSAGE.REVIEW_DELETE_ONE, { reviewId }).pipe(
-        catchError((error: IRpcException) => {
-          throw new HttpException(error.message, error.status)
-        })
-      )
+      this.reviewsClient
+        .send(QUEUE_MESSAGE.REVIEW_DELETE_ONE, { reviewId })
+        .pipe(
+          catchError((error: IRpcException) => {
+            throw new HttpException(error.message, error.status)
+          })
+        )
     )
   }
 }
