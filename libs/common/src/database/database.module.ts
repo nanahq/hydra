dc5358ffe622951adace9fc7d54c356bb140f22e
siradji <siradjiawoual@gmail.com>
2023-02-27 02:31:35 +0100

    feature(release): Deployed v1.8.17
    Signed-off-by: siradji <siradjiawoual@gmail.com>

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
-import { MongooseModule } from '@nestjs/mongoose'
+import { MongooseModule, MongooseModuleOptions } from '@nestjs/mongoose'
 
 @Module({
   imports: [
     MongooseModule.forRootAsync({
-      useFactory: (configService: ConfigService) => ({
+      useFactory: (configService: ConfigService): MongooseModuleOptions => ({
         uri:
           configService.get<string>('MONGODB_URI') ??
           'mongodb://root:password123@mongodb-primary:27017/'
