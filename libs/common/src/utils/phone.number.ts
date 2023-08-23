c0d981b5a51291ce2f1f5cc4015c64dcb8d5dd94
ahmard <ahmard.mu@gmail.com>
2023-08-23 11:21:15 +0100

    fix(vendor-status): corrected typo

--- libs/common/src/utils/phone.number.ts
+++ libs/common/src/utils/phone.number.ts
@@ -2,15 +2,15 @@ export function internationalisePhoneNumber (num: string): string {
   const length = num.length
 
   if (length === 10) { // 7011223344
-    return `234${num}`
+    return `+234${num}`
   }
 
   if (length === 11) { // 07011223344
-    return `234${num.slice(1)}`
+    return `+234${num.slice(1)}`
   }
 
-  if (length === 14) { // +2347011223344
-    return num.slice(1)
+  if (length === 13) { // +2347011223344
+    return `+${num}`
   }
 
   return num // 2347011223344
