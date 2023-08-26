d1849007eae72a18c05fc42e8b4377e834e81f11
ahmard <ahmard.mu@gmail.com>
2023-08-26 20:23:36 +0100

    fix(admin-service): un-expose admin service

--- apps/admin-service/src/main.ts
+++ apps/admin-service/src/main.ts
@@ -2,17 +2,14 @@ import { NestFactory } from '@nestjs/core'
 import { AdminServiceModule } from './admin-service.module'
 import { QUEUE_SERVICE, RmqService } from '@app/common'
 import { RmqOptions } from '@nestjs/microservices'
-import { ConfigService } from '@nestjs/config'
 
 async function bootstrap (): Promise<void> {
   const app = await NestFactory.create(AdminServiceModule)
-  const port = app.get(ConfigService).get<string>('PORT') as string
   const rmq = app.get<RmqService>(RmqService)
   app.connectMicroservice<RmqOptions>(
     rmq.getOption(QUEUE_SERVICE.ADMINS_SERVICE)
   )
   await app.startAllMicroservices()
-  await app.listen(port)
 }
 
 void bootstrap()
