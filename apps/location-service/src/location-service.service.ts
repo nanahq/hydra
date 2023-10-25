f63d37785553073963d16fa430d74931d70c8eef
Suraj Auwal <surajtauwal@gmail.com>
2023-10-25 16:25:56 +0100

    feature: delivery query vendors

--- apps/location-service/src/location-service.service.ts
+++ apps/location-service/src/location-service.service.ts
@@ -39,8 +39,8 @@ export class LocationService {
     destination: number[]
   ): Promise<TravelDistanceResult> {
     const url = `https://api.mapbox.com/directions/v5/mapbox/driving/${
-      origin[0]
-    },${origin[1]};${destination[0]},${destination[1]}?access_token=${
+      origin[1]
+    },${origin[0]};${destination[1]},${destination[0]}?access_token=${
       this.mapboxToken as string
     }`
     try {
