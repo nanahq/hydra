d253002b297ab747364193e63fecf3b3fb5c3b78
Suraj Auwal <surajtauwal@gmail.com>
2023-11-16 17:29:47 +0100

    feature: emit location to delivery room

--- apps/drivers-service/src/websockets/events.gateway.ts
+++ apps/drivers-service/src/websockets/events.gateway.ts
@@ -1,10 +1,10 @@
 import {
-  MessageBody,
+  MessageBody, OnGatewayConnection,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer
 } from '@nestjs/websockets'
-import { Server } from 'socket.io'
+import { Server, Socket } from 'socket.io'
 import { LocationCoordinates, SOCKET_MESSAGE } from '@app/common'
 import { Logger } from '@nestjs/common'
 import { EventsService } from './events.service'
@@ -14,30 +14,41 @@ import { EventsService } from './events.service'
     origin: '*'
   }
 })
-export class EventsGateway {
+export class EventsGateway implements OnGatewayConnection {
   @WebSocketServer()
     server: Server
 
   private readonly logger = new Logger(EventsGateway.name)
 
-  constructor (
-    private readonly eventService: EventsService
-  ) {}
+  constructor (private readonly eventService: EventsService) {}
+
+  async handleConnection (client: Socket, ...args: any[]): Promise<void> {
+    const deliveryId = client?.handshake?.query?.deliveryId as string | null
+    if (deliveryId !== null) {
+      await client.join(deliveryId)
+      this.logger.log(`Client connected to delivery room: ${deliveryId}`)
+    }
+  }
 
   @SubscribeMessage(SOCKET_MESSAGE.UPDATE_DRIVER_LOCATION)
   async updateDriversLocation (
-    @MessageBody() { driverId, location }: { driverId: string, location: LocationCoordinates }
+    @MessageBody()
+      { driverId, location }: { driverId: string, location: LocationCoordinates }
   ): Promise<void> {
     try {
-      if (driverId === undefined || location === undefined) {
-        throw new Error('failed')
-      }
       await this.eventService.updateDriverLocation(driverId, location)
-      await this.eventService.updateDeliveryLocation(driverId, location)
+
+      const updatedDeliveryId = await this.eventService.updateDeliveryLocation(driverId, location)
+
+      if (updatedDeliveryId !== null) {
+        this.server.to(updatedDeliveryId).emit(SOCKET_MESSAGE.DRIVER_LOCATION_UPDATED, {
+          driverId,
+          location
+        })
+      }
     } catch (error) {
-      this.logger.error(
-        `PIM -> Failed to update location for driver ${driverId}`
-      )
+      this.logger.error(JSON.stringify(error))
+      this.logger.error(`Failed to update location for driver ${driverId}`)
     }
   }
 }
--- apps/drivers-service/src/websockets/events.service.ts
+++ apps/drivers-service/src/websockets/events.service.ts
@@ -1,6 +1,6 @@
 import { HttpStatus, Injectable, Logger } from '@nestjs/common'
 import { OdsaRepository } from '../ODSA/odsa.repository'
-import { FitRpcException, LocationCoordinates, OrderStatus } from '@app/common'
+import { Delivery, FitRpcException, LocationCoordinates, OrderStatus } from '@app/common'
 import { DriverRepository } from '../drivers-service.repository'
 
 @Injectable()
@@ -12,9 +12,10 @@ export class EventsService {
 
   ) {}
 
-  async updateDeliveryLocation (driverId: string, location: LocationCoordinates): Promise<void> {
+  async updateDeliveryLocation (driverId: string, location: LocationCoordinates): Promise<any> {
     try {
-      await this.odsaRepository.findOneAndUpdate({ assignedToDriver: true, driver: driverId, status: OrderStatus.IN_ROUTE}, { currentLocation: location })
+      const delivery: Delivery | null = await this.odsaRepository.findOneAndUpdate({ assignedToDriver: true, completed: false, driver: driverId, status: OrderStatus.IN_ROUTE }, { currentLocation: location })
+      return delivery?._id
     } catch (error) {
       throw new FitRpcException('failed to update Delivery location. Something went wrong', HttpStatus.INTERNAL_SERVER_ERROR)
     }
