769d2d806cbd35bd6743cfb2f20fc086f455b534
Suraj Auwal <siradjiawoual@gmail.com>
2024-08-20 12:29:57 +0100

    chore: merge development to s (#453)
    * feature(app): added sentry monitoring (#444)
    
    * chore: fix failing build command (#441)
    
    * chore: merge staging into main (#442)
    
    * chore: fix failing build command
    
    * chore: fix failing build command
    
    * chore: removed rennovate bot
    
    * refactor: removed redundant flutterwave code
    
    * refactore: removed flutterwave sdk
    
    * chore: k8s manifest files + gh action ovh deployment
    
    * feature: added sentry monitoring
    
    * feature(notification): create TermiiService for sending and verifying otps (#448)
    
    * chore: fix failing build command (#441)
    
    * chore: merge staging into main (#442)
    
    * chore: fix failing build command
    
    * chore: fix failing build command
    
    * chore: removed rennovate bot
    
    * refactor: removed redundant flutterwave code
    
    * refactore: removed flutterwave sdk
    
    * chore: k8s manifest files + gh action ovh deployment
    
    * feature(notification): Add TermiiService for sending and verifying SMS tokens
    
    ---------
    
    Co-authored-by: Suraj Auwal <siradjiawoual@gmail.com>
    
    * feature: updated s3 upload credentials (#450)
    
    * chore: fix failing build command (#441)
    
    * chore: merge staging into main (#442)
    
    * chore: fix failing build command
    
    * chore: fix failing build command
    
    * chore: removed rennovate bot
    
    * refactor: removed redundant flutterwave code
    
    * refactore: removed flutterwave sdk
    
    * chore: k8s manifest files + gh action ovh deployment
    
    * feature: merge staging into main  (#446)
    
    * chore: fix failing build command
    
    * chore: fix failing build command
    
    * chore: removed rennovate bot
    
    * refactor: removed redundant flutterwave code
    
    * refactore: removed flutterwave sdk
    
    * chore: k8s manifest files + gh action ovh deployment
    
    * feature(app): added sentry monitoring (#444) (#445)
    
    * chore: fix failing build command (#441)
    
    * chore: merge staging into main (#442)
    
    * chore: fix failing build command
    
    * chore: fix failing build command
    
    * chore: removed rennovate bot
    
    * refactor: removed redundant flutterwave code
    
    * refactore: removed flutterwave sdk
    
    * chore: k8s manifest files + gh action ovh deployment
    
    * feature: added sentry monitoring
    
    * chore: changed target branch to main from
    
    * chore(merge): merge development unto staging (#449)
    
    * feature(app): added sentry monitoring (#444)
    
    * chore: fix failing build command (#441)
    
    * chore: merge staging into main (#442)
    
    * chore: fix failing build command
    
    * chore: fix failing build command
    
    * chore: removed rennovate bot
    
    * refactor: removed redundant flutterwave code
    
    * refactore: removed flutterwave sdk
    
    * chore: k8s manifest files + gh action ovh deployment
    
    * feature: added sentry monitoring
    
    * feature(notification): create TermiiService for sending and verifying otps (#448)
    
    * chore: fix failing build command (#441)
    
    * chore: merge staging into main (#442)
    
    * chore: fix failing build command
    
    * chore: fix failing build command
    
    * chore: removed rennovate bot
    
    * refactor: removed redundant flutterwave code
    
    * refactore: removed flutterwave sdk
    
    * chore: k8s manifest files + gh action ovh deployment
    
    * feature(notification): Add TermiiService for sending and verifying SMS tokens
    
    ---------
    
    Co-authored-by: Suraj Auwal <siradjiawoual@gmail.com>
    
    ---------
    
    Co-authored-by: Samuel Theophilus <105337973+afkzoro@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Samuel Theophilus <105337973+afkzoro@users.noreply.github.com>
    
    * feature: updated s3 upload credentials
    
    ---------
    
    Co-authored-by: Samuel Theophilus <105337973+afkzoro@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Samuel Theophilus <105337973+afkzoro@users.noreply.github.com>

--- apps/vendor-gateway/src/aws.service.ts
+++ apps/vendor-gateway/src/aws.service.ts
@@ -2,16 +2,18 @@ import { Injectable, Logger } from '@nestjs/common'
 import { PutObjectCommand, S3 } from '@aws-sdk/client-s3'
 import { fromEnv } from '@aws-sdk/credential-providers'
 import { RandomGen } from '@app/common'
+import { ConfigService } from '@nestjs/config'
 @Injectable()
 export class AwsService {
   private readonly logger = new Logger(AwsService.name)
   protected readonly AWS_S3_BUCKET: string
   public s3: S3
-  constructor () {
-    this.AWS_S3_BUCKET = 'nana-ng'
+  constructor (private readonly configService: ConfigService) {
+    const endpoint = this.configService.get('AWS_S3_ENDPOINT')
+    this.AWS_S3_BUCKET = 'nana-vendors'
     this.s3 = new S3({
       credentials: fromEnv(),
-      region: 'af-south-1'
+      endpoint
     })
   }
 
@@ -29,7 +31,7 @@ export class AwsService {
       ACL: 'public-read'
     })
 
-    const url = `https://${this.AWS_S3_BUCKET}.s3.af-south-1.amazonaws.com/${Key}`
+    const url = `https://nana-vendors.s3.uk.io.cloud.ovh.net/${Key}`
     try {
       await this.s3.send(command)
       return url
